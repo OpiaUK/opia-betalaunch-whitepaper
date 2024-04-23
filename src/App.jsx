9a7cba5db345dfea00c0cbac8d2f402cbb9ec093
Andrew <167503082+OpiaUK@users.noreply.github.com>
2024-04-23 01:21:18 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="data">OPIA</h1>
+      <h2>
+        A Human-Centric Operating System That Powers The World Around You And
+        Beyond.
+      </h2>
+      <p>view the whitepaper</p>
+      <p>
+        https://docs.google.com/document/d/1IcBpwH_XpYzAxiEEiHttPfQPYIK7EBDOXbli6zIrj8A/edit?usp=sharing{" "}
+      </p>
+      <p>
+        buy tokens to vote on community agenda via telegram
+        https://t.me/thisisopia
+      </p>
+    </div>
+  );
+}
